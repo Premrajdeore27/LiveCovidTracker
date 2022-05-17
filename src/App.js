9e0e56f42d71455cb68cf261c12bbe34af218cb6
Premraj Deore <premrajdeore27@gmail.com>
2022-05-17 15:20:35 +0530

    init Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Assign_1 from './Component/Practice1/Assign_1'
+import Covid from "./Component/CovidTracker/Covid"
+const App = () => {
+  return (
+    <div>
+      
+      {/* <Assign_1/> */}
+      <Covid/>
+    </div>
+  )
+}
+
+export default App
